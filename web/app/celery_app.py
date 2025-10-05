e4086599fe409a7a45bc0853b8614179fc672467
Lucas Alves <lucas.varela@alura.com.br>
2025-10-05 10:26:15 -0300

    feat: adição do arquivo celery_app.py para configuração do Celery, incluindo tarefas agendadas para coleta de páginas populares e treinamento de modelo. A integração com o Flask foi implementada para permitir o uso do contexto da aplicação nas tarefas.

---
+++ web/app/celery_app.py
@@ -0,0 +1,44 @@
+import os
+from celery import Celery
+from datetime import datetime
+from .config import Config
+from .db import db
+from .models import ModelPrediction
+from .tmdb import collect_popular_pages
+from .ml import train_and_store
+from flask import Flask
+
+celery = Celery(__name__, broker=os.getenv("REDIS_URL"), backend=os.getenv("REDIS_URL"))
+
+celery.conf.beat_schedule = {
+    "fetch-tmdb-every-2-min": {
+        "task": "app.celery_app.task_ingest",
+        "schedule": 15.0,
+    },
+    "train-model-every-15-min": {
+        "task": "app.celery_app.task_train",
+        "schedule": 15.0*60.0,
+    },
+}
+
+
+def make_flask_app():
+    from . import create_app
+    return create_app()
+
+
+@celery.task(name="app.celery_app.task_ingest")
+def task_ingest():
+    app = make_flask_app()
+    with app.app_context():
+        res = collect_popular_pages(pages=2)
+        return res
+
+
+@celery.task(name="app.celery_app.task_train")
+def task_train():
+    app = make_flask_app()
+    with app.app_context():
+        res = train_and_store()
+        return res
+
